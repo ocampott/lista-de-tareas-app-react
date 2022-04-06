ff1daf92a9c81ed2923a88636ebc0cab950f360c
ocampott <ocampott7@gmail.com>
2022-04-05 21:03:32 -0300

    Fixed some CSS things

--- lista-de-tareas-app/src/components/Task.js
+++ lista-de-tareas-app/src/components/Task.js
@@ -10,7 +10,7 @@ export const Task = ({task, toggleCompleted, editTaskText, deleteTask}) => {
     return (
     <li className="task-list__task">
         <FontAwesomeIcon onClick={() => toggleCompleted(task.id)} icon={task.completed ? faCheckSquare : faSquare} className="task-list__icon task-list__icon-check"/>
-        <div className={ !task.completed ? "task-list__list__text-line" : "task-list__text-completed"}>
+        <div className="task-list__text">
             {editTask ? 
                 <EditTask task={ task } setEditTask={ setEditTask } editTaskText={ editTaskText }/>
             :
